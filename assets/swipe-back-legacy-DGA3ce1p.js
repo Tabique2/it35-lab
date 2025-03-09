5833913625d8be1d1ad854a5b299e5cbabd4b747
Tabique2 <franktabique10@gmail.com>
2025-03-09 13:22:51 +0800

    Updates

--- assets/swipe-back-legacy-DGA3ce1p.js
+++ assets/swipe-back-legacy-DGA3ce1p.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-tnXhqLou.js","./index3-legacy-XqIF9Kbs.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Dg3-C1Ub.js","./index3-legacy-sM_9CAXT.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
