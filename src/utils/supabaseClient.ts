f6f513a20f9aee4d7147dec58bc8c767b702eff3
Tabique2 <franktabique10@gmail.com>
2025-04-02 23:50:31 +0800

    Added supabase and bycrypt

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL='https://xoukgtxswrzwafbljipr.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvdWtndHhzd3J6d2FmYmxqaXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1OTYzNzksImV4cCI6MjA1OTE3MjM3OX0.TCpBnvnh_pS5juRBiDNfMB1qxbvcFRfWlIyKHdS7xw8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
