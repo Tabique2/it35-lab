428f58e7a14325f5dbeb5c3e072f38c8988f9f58
Tabique2 <franktabique10@gmail.com>
2025-04-30 21:47:56 +0800

    Updates

--- assets/ios.transition-legacy-D8852c_C.js
+++ assets/ios.transition-legacy-D8852c_C.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D3uXVkQi.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CA0tboKf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BPe5L5KK.js
+++ assets/md.transition-legacy-BPe5L5KK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D3uXVkQi.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CA0tboKf.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
